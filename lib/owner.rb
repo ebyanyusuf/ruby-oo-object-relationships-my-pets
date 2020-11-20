32c0a53c89ca9e58b2057ab29ef2c32def4eaf0d
ebyanyusuf <ebyan.yusuf@gmail.com>
2020-11-20 12:13:46 -0500

    commit tree a5eb6473a148eb8d951398ad75d7e2c6f70b9dec

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
+
+
 class Owner
-  # code goes here
+  attr_reader :name
+  attr_reader :species
+  @@Owners = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@Owners << self
+end
+
+def say_species
+  p "I am a #{@species}."
+end
+
+def self.all
+  p @@Owners
+end
+
+def self.count
+  p self.all.count
+end
+
+
 end
